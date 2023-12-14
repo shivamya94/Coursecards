44e175e58ece2d8df68f7ba50fb84c496d2e0062
Shivamya94 <yadavshivam90440@gmail.com>
2023-12-15 01:06:57 +0530

    start Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter"
+import Cards from "./components/Cards"
+import { apiUrl,filterData } from "./data";
+import { useState,useEffect } from "react";
+import Spinner from "./components/Spinner";
+import {toast} from "react-toastify";
 
-function App() {
+const App = () => {
+  const [courses,setCourses] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const [category,setCategory] = useState(filterData[0].title);
+
+  async function fetchData () {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      ///output ->
+      setCourses(output.data);
+    }
+    catch(error) {
+      toast.error("Network me koi dikkat hai");
+    }
+    setLoading(false);
+  } 
+
+  useEffect( () =>{
+    fetchData();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col">
+      <div>
+        <Navbar/>
+      </div>
+      <div className="bg-slate-500">
+       <div>
+         <Filter 
+         filterData={filterData}
+         category={category}
+         setCategory={setCategory}/>
+       </div>
+       <div className="w-11/12 max-w-[1200px]
+       max-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+      {
+          loading ? (<Spinner/>) : (<Cards courses={courses} category = {category}/>)
+        }
+      </div>
+
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
